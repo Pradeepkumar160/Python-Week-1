96ebd5b7b84d91fa1b4d0d0a1697ec2eee632805
Pradeepkumar160 <115738337+Pradeepkumar160@users.noreply.github.com>
2022-12-11 12:36:50 +0530

    Create class30-StepArguement-CipherSchools.py

---
+++ class30-StepArguement-CipherSchools.py
@@ -0,0 +1,9 @@
+#slicing /selecting subsequences
+lang = "python"
+#syntax - [start arguement: stop arguement:step]
+print("Pradeep"[2:5])     #output= ade
+print("Pradeep"[2:5:1])   #output= ade
+print("Pradeep"[2:5:2])   #output= ae 
+print("Pradeep"[0::2])    #output= Paep
+print("Pradeep"[8::-1])   #output = peedarP
+print("Pradeep"[::-1])    #output = peedarP (TRICK)
