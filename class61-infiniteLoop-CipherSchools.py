b769fc7c1bf3da7e839b622de71afaa194c79635
Pradeepkumar160 <115738337+Pradeepkumar160@users.noreply.github.com>
2022-12-11 15:41:21 +0530

    Create class61-infiniteLoop-CipherSchools.py

---
+++ class61-infiniteLoop-CipherSchools.py
@@ -0,0 +1,9 @@
+# infinite loop
+i = 0 
+while i <=10:
+    print("hello world")    # infinite loop will continue
+
+
+#2. method for infinite loop 
+while True :               #t of true will be capital
+    print("hello world")
